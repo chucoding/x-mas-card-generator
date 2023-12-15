857229887c46fb8a42aa8bad42b58c97d9ace8a4
chucoding <chucoding@gmail.com>
2023-12-16 08:29:11 +0900

    feat : swyg adv add on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,34 @@
 
 import "./globals.css";
 import Head from "./head";
+import Script from "next/script";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {}
+  }, []);
+
   return (
     <html lang="ko" style={{ fontFamily: "GangwonEdu_OTFBoldA" }}>
       <head>
         <Head />
+        <Script
+          defer
+          src="https://cdn.swygbro.com/public/widget/swyg-widget.js"
+        ></Script>
       </head>
       <body>
         <main
