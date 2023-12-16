5818acf1568b8ccf616fd4dd77e76ea2080bfee3
chucoding <chucoding@gmail.com>
2023-12-16 12:03:24 +0900

    feat : call HyperCLOVA X API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   experimental: {
     appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://clovastudio.stream.ntruss.com/:path*",
+      },
+    ];
   },
 };
 
--- src/components/templates/result.tsx
+++ src/components/templates/result.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Loading from "../../components/modules/Loading";
-import JSON from "../../resources/prompt.json";
+import json from "../../resources/prompt.json";
 
 interface ResultProps {
   data: {
-    select: keyof typeof JSON;
+    select: keyof typeof json;
     input: string;
     text: string;
   };
@@ -16,9 +16,53 @@ export default function Result({ data }: ResultProps) {
   const generate = async () => {
     setLoading(true);
     try {
-      console.log(JSON[data.select]);
+      const ncloud_api_options: RequestInit = {
+        method: "POST",
+        headers: {
+          "X-NCP-CLOVASTUDIO-API-KEY":
+            process.env.NEXT_PUBLIC_X_NCP_CLOVASTUDIO_API_KEY || "",
+          "X-NCP-APIGW-API-KEY":
+            process.env.NEXT_PUBLIC_X_NCP_APIGW_API_KEY || "",
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+          message: [
+            {
+              role: "system",
+              content: json[data.select],
+            },
+            {
+              role: "user",
+              content: "크리스마스 카드 만들어줘",
+            },
+          ],
+          topP: 0.8,
+          topK: 0,
+          maxTokens: 256,
+          temperature: 0.5,
+          repeatPenalty: 5.0,
+          stopBefore: [],
+          includeAiFilters: true,
+        }) as string,
+      };
+
+      const response = await fetch(
+        "/testapp/v1/chat-completions/HCX-002",
+        ncloud_api_options
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const resultData = await response.json();
+      console.log(resultData);
+
+      // 다음 작업 수행...
     } catch (err) {
-      //alert(err);
+      console.error("Error during fetch:", err);
+    } finally {
+      setLoading(false);
     }
   };
 
