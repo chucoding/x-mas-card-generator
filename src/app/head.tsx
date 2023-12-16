d03e2132bc762e6a1e526f6051261de70b923c66
chucoding <chucoding@gmail.com>
2023-12-16 22:05:01 +0900

    chore : meta tag & favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,8 +2,8 @@ export default function Head() {
   const title = "크리스마스 카드 만들기";
   const description =
     "HyperCLOVA X 모델을 이용해 크리스마스 카드를 자동으로 생성해주는 웹 사이트";
-  const url = "https://xmas.swygbro.com";
-  const thumbnail = `${url}/img/thumbnail.png`;
+  const url =
+    "https://x-mas-card-generator-byjgfzwv8-chucodings-projects.vercel.app";
 
   return (
     <>
@@ -19,16 +19,16 @@ export default function Head() {
       <meta name="og:description" content={description} />
       <meta name="og:type" content="website" />
       <meta name="og:url" content={url} />
-      <meta name="og:image" content={thumbnail}></meta>
+      <meta name="og:image" content="/favicon.ico"></meta>
       <meta name="twitter:site_name" content={title} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:type" content="website" />
       <meta name="twitter:url" content={url} />
-      <meta name="twitter:image" content={thumbnail}></meta>
+      <meta name="twitter:image" content="/favicon.ico"></meta>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href={thumbnail} />
-      <link rel="shortcut icon" href={thumbnail} />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </>
   );
 }
