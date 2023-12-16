b096c125c6298a36e77668ed9a66f8b99e25d4c9
chucoding <chucoding@gmail.com>
2023-12-16 18:01:00 +0900

    style : loading page text design

--- src/components/modules/Loading.tsx
+++ src/components/modules/Loading.tsx
@@ -8,8 +8,9 @@ export default function Loading(): JSX.Element {
       className="inline-block text-center w-full h-full bg-red-400"
       style={{ fontFamily: "KCCChassam" }}
     >
-      <h1 className="absolute top-[16%] text-[45px] text-white animate-fadein">
-        카드를 만들고 있습니다...
+      <h1 className="absolute top-[26%] left-[5%] text-[26px] text-white text-center animate-fadein">
+        도우미가 카드를 만들기 시작했어요
+        <br /> 잠시만 기다려주세요🪄
       </h1>
       <Lottie
         className="absolute top-[36%] left-[10%]"
