2a199c50834ebd611c472f85ed09d1aded1434a2
chucoding <chucoding@gmail.com>
2023-12-27 10:50:08 +0900

    feat : google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,22 @@ export default function RootLayout({
         >
           <div className="text-center w-[375px] h-full relative">
             <div className="w-full h-full absolute top-0 bottom-0 align-middle">
+              <div className="absolute top-0">
+                <Script
+                  async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1919598055512436"
+                  crossOrigin="anonymous"
+                ></Script>
+                <ins
+                  className="adsbygoogle"
+                  style={{
+                    display: "inline-block",
+                    height: "100px",
+                  }}
+                  data-ad-client="ca-pub-1919598055512436"
+                  data-ad-slot="1678485541"
+                ></ins>
+              </div>
               {children}
             </div>
           </div>
